e2eed9a652e40fee3cccdcbf86af0b42f09f2f6d
KANAjetzt <kai@kana.jetzt>
2021-01-19 10:59:44 +0100

    ✅ Backend connection working

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,10 @@
 <script>
   import * as Colyseus from "colyseus.js";
+  import AutoNumeric from "AutoNumeric";
+  import { onMount } from "svelte";
+
   import { roomState } from "./stores";
+  import Currency from "./Currency.svelte";
 
   const client = new Colyseus.Client("ws://localhost:2567");
 
@@ -17,23 +21,39 @@
     // listen to state change
     room.onStateChange(state => {
       $roomState = state;
-      console.log(room.name, "has new state:", state);
       console.log($roomState);
     });
   };
 
-  let roomStatee;
+  onMount(() => {
+    // Add formating to currency input
+    new AutoNumeric(".guessedPriceInput", {
+      currencySymbol: "€",
+      currencySymbolPlacement: "s"
+    });
 
-  handleRoom();
+    // Connect to game room and listen for state change
+    handleRoom();
+  });
 </script>
 
 <style>
-
+  img {
+    max-width: 100vw;
+  }
 </style>
 
 <main>
-  {console.log($roomState)}
-  {#if $roomState && $roomState.prodcutTitle}
-    <h2>{$roomState.productTitle}</h2>
+  {#if $roomState}
+    <h2>{$roomState.currentProduct.productTitle}</h2>
+    <img src={$roomState.currentProduct.productImg} alt="product" />
+    <p>{$roomState.currentProduct.productDescription}</p>
+    <p>{$roomState.currentProduct.productStars}</p>
+    <p>
+      <Currency cent={$roomState.currentProduct.productPrice} />
+    </p>
   {/if}
+
+  <input type="text" class="guessedPriceInput" />
+  <button>✔</button>
 </main>
---
+++ src/Currency.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let cent;
+
+  const handleEuro = cent => {
+    return cent / 100;
+  };
+</script>
+
+{`${handleEuro(cent)}€`}
