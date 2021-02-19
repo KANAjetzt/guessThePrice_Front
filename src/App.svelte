36f3104a5cb03057deb51dc038ce234f823e4e87
KANAjetzt <kai@kana.jetzt>
2021-02-19 21:17:03 +0100

    🚀 1.1.1 launch prep

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,7 @@
   import Carousel from "./components/Carousel.svelte";
   import CurrencyInput from "./components/CurrencyInput.svelte";
 
-  const client = new Colyseus.Client("ws://192.168.178.34:2567");
+  const client = new Colyseus.Client("ws://guessthepricegame.herokuapp.com/");
 
   const joinRoom = async () => {
     const room = await client.joinOrCreate("my_room");
