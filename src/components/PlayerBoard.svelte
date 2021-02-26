da1bed08100756ecde4eadaa38e65e490162bd6b
KANAjetzt <kai@kana.jetzt>
2021-02-26 11:47:28 +0100

    ✔ Show round score inbetween rounds

--- src/components/PlayerBoard.svelte
+++ src/components/PlayerBoard.svelte
@@ -1,26 +1,23 @@
 <script>
   import { roomState } from "../stores.js";
   import PlayerCard from "./PlayerCard.svelte";
-
-  $: if ($roomState) {
-    $roomState.playerStates.$items.forEach(e => {});
-  }
 </script>
+
+<div class="playerBoard">
+  {#if $roomState}
+    {#each [...$roomState.playerStates.$items] as player}
+      <PlayerCard
+        name={player[1].name}
+        roundScore={player[1].roundScore}
+        guessedPrice={player[1].guessedPrice}
+        score={player[1].score}
+      />
+    {/each}
+  {/if}
+</div>
 
 <style>
   .playerBoard {
     border: solid 3px black;
   }
 </style>
-
-<div class="playerBoard">
-  {#if $roomState}
-    {#each [...$roomState.playerStates.$items] as player}
-      <PlayerCard
-        id={player[1].id}
-        name={player[1].name}
-        guessedPrice={player[1].guessedPrice}
-        score={player[1].score} />
-    {/each}
-  {/if}
-</div>
