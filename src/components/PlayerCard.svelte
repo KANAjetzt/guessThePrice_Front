bebd8bbc59399d956e8b7cac14fca1c32a54650b
KANAjetzt <kai@kana.jetzt>
2021-03-01 12:42:11 +0100

    ✔ roundscore animation

--- src/components/PlayerCard.svelte
+++ src/components/PlayerCard.svelte
@@ -1,14 +1,27 @@
 <script>
+  // - Player Name
+  // - Player Avatar (later)
+  // - Guesed Price
+  // - Current Score
+
+  import { tweened } from "svelte/motion";
+  import { cubicInOut } from "svelte/easing";
+  import { fade, fly } from "svelte/transition";
   import Currency from "./Currency.svelte";
 
   export let name;
   export let roundScore = 0;
   export let score = 0;
   export let guessedPrice = 0;
-  // - Player Name
-  // - Player Avatar (later)
-  // - Guesed Price
-  // - Current Score
+
+  let showRoundScore = true;
+
+  const tweenedScore = tweened(score - roundScore, {
+    duration: 4000,
+    easing: cubicInOut,
+  });
+
+  $: tweenedScore.set(score);
 </script>
 
 {#if name}
@@ -19,14 +32,24 @@
   <Currency cent={guessedPrice} />
 </p>
 <p>
-  {score}
-  {#if roundScore !== 0}
-    <span class="roundScore"> +{roundScore}</span>
+  {Math.floor($tweenedScore)}
+  {#if roundScore !== 0 && showRoundScore}
+    <span
+      class="roundScore"
+      in:fade={{ duration: 300 }}
+      out:fly={{ duration: 1500, delay: 2000, x: -30 }}
+      on:introend={() => {
+        showRoundScore = false;
+      }}
+    >
+      +{roundScore}</span
+    >
   {/if}
 </p>
 
 <style>
   .roundScore {
+    display: inline-block;
     color: green;
   }
 </style>
