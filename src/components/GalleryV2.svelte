5cdc27070cee3d50ce82da2af1419c066200b88c
Kai Roth <kai@kana.jetzt>
2021-04-17 17:19:28 +0200

    🔨 changed hover effect

--- src/components/GalleryV2.svelte
+++ src/components/GalleryV2.svelte
@@ -7,17 +7,15 @@
 
 <div class="gallery">
   {#each [...$roomState.currentProduct.imgs.$items.get(0).mediumImgs] as src, i}
-    <div class="imgContainer">
-      <img
-        class="img"
-        {src}
-        alt={`Productbild-${i}`}
-        on:click={() => {
-          $appStore.modalIsOpen = true;
-          $appStore.galleryClickedImage = i;
-        }}
-      />
-    </div>
+    <img
+      class="img"
+      {src}
+      alt={`Productbild-${i}`}
+      on:click={() => {
+        $appStore.modalIsOpen = true;
+        $appStore.galleryClickedImage = i;
+      }}
+    />
   {/each}
 </div>
 
@@ -30,26 +28,17 @@
     padding: 0 1.4rem;
   }
 
-  .imgContainer {
+  .img {
     width: 15rem;
     height: 15rem;
-    overflow: hidden;
-    transition: all 0.2s ease-out;
-  }
-
-  .imgContainer:hover {
-    filter: drop-shadow(var(--shadow-1--color1));
-  }
-
-  .img {
-    width: 100%;
-    height: 100%;
     object-fit: cover;
     transition: all 0.2s ease-out;
     cursor: pointer;
+    backface-visibility: hidden;
   }
 
   .img:hover {
     transform: scale(1.05);
+    filter: drop-shadow(var(--shadow-1--color1));
   }
 </style>
