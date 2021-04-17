7e661e0b77e2410eeae13bc99c479da833379851
Kai Roth <kai@kana.jetzt>
2021-04-17 22:50:47 +0200

    ✔🛠 randomName Refresh button

--- src/components/TextInput.svelte
+++ src/components/TextInput.svelte
@@ -1,9 +1,23 @@
 <script>
+  import { roomState, roomStore, appStore } from "../stores";
+  import IconReplay from "./Icons/Replay.svelte";
+
   export let value;
+
+  const handleRandomNameReload = async () => {
+    // Send random name cange message to BE
+    await $roomStore.send("randomNameChange");
+    console.log("randomNameChange");
+  };
 </script>
 
 <div class="border">
-  <input class="input" type="text" bind:value />
+  <div class="inputWrapper">
+    <div class="icon" on:click={handleRandomNameReload}>
+      <IconReplay width={20} height={20} fill={"#f2e527"} />
+    </div>
+    <input class="input" type="text" bind:value />
+  </div>
 </div>
 
 <style>
@@ -13,6 +27,10 @@
     background-image: var(--color-gradient-2);
     padding: 0.2rem;
     border-radius: 3px;
+  }
+
+  .inputWrapper {
+    position: relative;
   }
 
   .input {
@@ -28,5 +46,13 @@
   .input:hover,
   .input:focus {
     filter: contrast(1.5);
+  }
+
+  .icon {
+    position: absolute;
+    top: 54%;
+    right: -2%;
+    transform: translate(-50%, -50%);
+    z-index: 5;
   }
 </style>
